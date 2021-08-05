874784a825d29eda96b335c74ba14cb6c6fcb38b
Vitor Morelli <vitao.miacri@gmail.com>
2021-08-04 22:23:36 -0300

    chore: initial project config

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*{ts,tsx}',
+    '!<rootDir>/src/main/**/*',
+    '!<rootDir>/src/domain/**/index.ts',
+    '!<rootDir>/src/presentation/components/router/**/*',
+    '!<rootDir>/src/**/index.ts',
+    '!**/*.d.ts'
+  ],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'jsdom',
+  transform: {
+    '.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1',
+    '\\.scss$': 'identity-obj-proxy',
+  },
+};
