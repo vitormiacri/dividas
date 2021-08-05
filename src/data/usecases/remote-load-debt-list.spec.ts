7db2f81f5570ca68d086e61f80a76d525ab104a7
Vitor Morelli <vitao.miacri@gmail.com>
2021-08-05 09:27:33 -0300

    test: ensure RemoteLoadDebtList throws UnexpectedError if HttpPostclient returns 400

--- src/data/usecases/remote-load-debt-list.spec.ts
+++ src/data/usecases/remote-load-debt-list.spec.ts
@@ -1,5 +1,7 @@
 import faker from 'faker';
+import { UnexpectedError } from '@/domain/erros/unexpected-error';
 import { RemoteDebtModel } from '../models/remote-debt-model';
+import { HttpStatusCode } from '../protocols/http';
 import { HttpClientSpy } from '../test/mock-http';
 import { RemoteLoadDebtList } from './remote-load-debt-list';
 
@@ -26,5 +28,17 @@ describe('RemoteLoadDebtList', () => {
 
     expect(httpClientSpy.url).toBe(url);
     expect(httpClientSpy.method).toBe('get');
+  });
+
+  test('Should throw UnexpectedError if HttpClient returns 400', async () => {
+    const url = faker.internet.url();
+    const { sut, httpClientSpy } = makeSut(url);
+    httpClientSpy.response = {
+      statusCode: HttpStatusCode.badRequest,
+    };
+
+    const promise = sut.loadAll();
+
+    await expect(promise).rejects.toThrow(new UnexpectedError());
   });
 });
