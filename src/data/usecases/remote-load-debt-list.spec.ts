573baf08fa9b84f77e8dea1b354a71b925522342
Vitor Morelli <vitao.miacri@gmail.com>
2021-08-05 09:53:05 -0300

    test: ensure RemoteLoadDebtList returns a list of DebtModels if HttpPostclient returns 200

--- src/data/usecases/remote-load-debt-list.spec.ts
+++ src/data/usecases/remote-load-debt-list.spec.ts
@@ -1,8 +1,9 @@
 import faker from 'faker';
 import { UnexpectedError } from '@/domain/erros/unexpected-error';
-import { RemoteDebtModel } from '../models/remote-debt-model';
-import { HttpStatusCode } from '../protocols/http';
-import { HttpClientSpy } from '../test/mock-http';
+import { RemoteDebtModel } from '@/data/models/remote-debt-model';
+import { HttpStatusCode } from '@/data/protocols/http';
+import { HttpClientSpy } from '@/data/test/mock-http';
+import { mockRemoteDebtListModel } from '@/data/test/mock-remote-debt-list';
 import { RemoteLoadDebtList } from './remote-load-debt-list';
 
 type SutTypes = {
@@ -31,8 +32,7 @@ describe('RemoteLoadDebtList', () => {
   });
 
   test('Should throw UnexpectedError if HttpClient returns 400', async () => {
-    const url = faker.internet.url();
-    const { sut, httpClientSpy } = makeSut(url);
+    const { sut, httpClientSpy } = makeSut();
     httpClientSpy.response = {
       statusCode: HttpStatusCode.badRequest,
     };
@@ -40,5 +40,37 @@ describe('RemoteLoadDebtList', () => {
     const promise = sut.loadAll();
 
     await expect(promise).rejects.toThrow(new UnexpectedError());
+  });
+
+  test('Should return a list of DebtModels if HttpClient returns 200', async () => {
+    const { sut, httpClientSpy } = makeSut();
+    const httpResult = mockRemoteDebtListModel();
+    httpClientSpy.response = {
+      statusCode: HttpStatusCode.ok,
+      body: httpResult,
+    };
+
+    const debtList = await sut.loadAll();
+
+    expect(debtList).toEqual([
+      {
+        idUsuario: httpResult[0].idUsuario,
+        motivo: httpResult[0].motivo,
+        valor: httpResult[0].valor,
+        data: new Date(httpResult[0].data),
+      },
+      {
+        idUsuario: httpResult[1].idUsuario,
+        motivo: httpResult[1].motivo,
+        valor: httpResult[1].valor,
+        data: new Date(httpResult[1].data),
+      },
+      {
+        idUsuario: httpResult[2].idUsuario,
+        motivo: httpResult[2].motivo,
+        valor: httpResult[2].valor,
+        data: new Date(httpResult[2].data),
+      },
+    ]);
   });
 });
