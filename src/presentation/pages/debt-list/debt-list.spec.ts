ae77f44ba831cbc846a958c49122b5a73de38095
Vitor Morelli <vitao.miacri@gmail.com>
2021-08-05 17:42:02 -0300

    refactor: move SUT creation to a factory method

--- src/presentation/pages/debt-list/debt-list.spec.ts
+++ src/presentation/pages/debt-list/debt-list.spec.ts
@@ -2,17 +2,31 @@ import { LoadDebtListSpy } from '@/domain/test/mock-debt-list';
 import { renderWithHistory } from '@/presentation/test/render-helper';
 import DebtList from './debt-list';
 
-import { createMemoryHistory } from 'history';
-import { waitFor, screen } from '@testing-library/react';
+import { createMemoryHistory, MemoryHistory } from 'history';
+import { waitFor, screen, RenderResult } from '@testing-library/react';
+
+type SutTypes = {
+  history: MemoryHistory;
+  loadDebtListSpy: LoadDebtListSpy;
+  sut: RenderResult;
+};
+
+const makeSut = (loadDebtListSpy = new LoadDebtListSpy()): SutTypes => {
+  const history = createMemoryHistory({ initialEntries: ['/'] });
+  const { sut } = renderWithHistory({
+    history,
+    Page: () => DebtList({ loadDebtList: loadDebtListSpy }),
+  });
+  return {
+    sut,
+    history,
+    loadDebtListSpy,
+  };
+};
 
 describe('DebtList', () => {
   test('Should render DebtItems on success', async () => {
-    const history = createMemoryHistory({ initialEntries: ['/'] });
-    const loadDebtListSpy = new LoadDebtListSpy();
-    renderWithHistory({
-      history,
-      Page: () => DebtList({ loadDebtList: loadDebtListSpy }),
-    });
+    const { loadDebtListSpy } = makeSut();
     expect(loadDebtListSpy.callsCount).toBe(1);
     await waitFor(() => screen.getByRole('heading'));
   });
