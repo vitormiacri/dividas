068e58fc5597dd9f8245e3d9c51719655efeb80a
Vitor Morelli <vitao.miacri@gmail.com>
2021-08-05 10:59:37 -0300

    feat: ensure AxiosHttpClient return correct response

--- src/data/protocols/http/http-client.ts
+++ src/data/protocols/http/http-client.ts
@@ -2,6 +2,7 @@ export type HttpRequest = {
   url: string;
   method: HttpMethod;
   body?: any;
+  params?: any;
 };
 
 export interface HttpClient<R = any> {
---
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -0,0 +1,24 @@
+import { HttpClient, HttpRequest, HttpResponse } from '@/data/protocols/http';
+import axios, { AxiosResponse } from 'axios';
+
+export class AxiosHttpClient implements HttpClient {
+  async request(data: HttpRequest): Promise<HttpResponse> {
+    let axiosResponse: AxiosResponse;
+    try {
+      axiosResponse = await axios.request({
+        url: data.url,
+        method: data.method,
+        data: data.body,
+        params: {
+          uuid: 'f8fc3eeb-d0e7-4462-9c62-408a4435bd8e',
+        },
+      });
+    } catch (error) {
+      axiosResponse = error.response;
+    }
+    return {
+      statusCode: axiosResponse.status,
+      body: axiosResponse.data,
+    };
+  }
+}
