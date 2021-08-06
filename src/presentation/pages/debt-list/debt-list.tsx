35dacc7e362a9d27ea769b44e746bfbce47c290c
Vitor Morelli <vitao.miacri@gmail.com>
2021-08-06 17:30:53 -0300

    feat: ensure DebtList render items on success

--- src/presentation/pages/debt-list/debt-list.tsx
+++ src/presentation/pages/debt-list/debt-list.tsx
@@ -1,17 +1,30 @@
+import { DebtModel } from '@/domain/models';
 import { LoadDebtList } from '@/domain/usecases';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Styles from './debt-list-styles.scss';
+
+type State = {
+  debts: DebtModel[];
+};
 
 type Props = {
   loadDebtList: LoadDebtList;
 };
 const DebtList: React.FC<Props> = ({ loadDebtList }) => {
+  const [state, setState] = useState<State>({
+    debts: [],
+  });
+
   useEffect(() => {
     loadDebtList.loadAll().then((debts) => {
-      console.log(debts);
+      setState((old) => ({
+        ...old,
+        debts,
+      }));
     });
   }, []);
+
   return (
     <div className={Styles.debtList}>
       <div className={Styles.card}>
@@ -28,31 +41,21 @@ const DebtList: React.FC<Props> = ({ loadDebtList }) => {
               <th>Data</th>
             </tr>
           </thead>
-          <tbody>
-            <tr>
-              <td>Cliente 1</td>
-              <td>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</td>
-              <td>R$ 10,00</td>
-              <td>01/08/2021</td>
-            </tr>
-            <tr>
-              <td>Cliente 2</td>
-              <td>Motivo 2</td>
-              <td>R$ 112,00</td>
-              <td>18/08/2021</td>
-            </tr>
-            <tr>
-              <td>Cliente 3</td>
-              <td>Motivo 3</td>
-              <td>R$ 210,00</td>
-              <td>15/08/2021</td>
-            </tr>
-            <tr>
-              <td>Cliente 4</td>
-              <td>Motivo 4</td>
-              <td>R$ 320,00</td>
-              <td>11/08/2021</td>
-            </tr>
+          <tbody data-testid="debt-list">
+            {state.debts.length > 0 ? (
+              state.debts.map((debt) => (
+                <tr key={debt.idUsuario}>
+                  <td>{debt.idUsuario}</td>
+                  <td>{debt.motivo}</td>
+                  <td>{debt.valor}</td>
+                  <td>{debt.data.toLocaleDateString('pt-br')}</td>
+                </tr>
+              ))
+            ) : (
+              <tr>
+                <td>Vazio</td>
+              </tr>
+            )}
           </tbody>
         </table>
       </div>
