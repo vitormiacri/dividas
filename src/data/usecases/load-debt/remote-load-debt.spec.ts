6d47a1d03a432de45ee1f5b3a0cef8facaea29d9
Vitor Morelli <vitao.miacri@gmail.com>
2021-08-07 15:09:26 -0300

    refactor: move SUT creation to a factory method

--- src/data/usecases/load-debt/remote-load-debt.spec.ts
+++ src/data/usecases/load-debt/remote-load-debt.spec.ts
@@ -2,12 +2,24 @@ import { HttpClientSpy } from '@/data/test';
 import faker from 'faker';
 import { RemoteLoadDebt } from './remote-load-debt.';
 
+type SutTypes = {
+  sut: RemoteLoadDebt;
+  httpClientSpy: HttpClientSpy;
+};
+const makeSut = (url = faker.internet.url()): SutTypes => {
+  const httpClientSpy = new HttpClientSpy();
+  const sut = new RemoteLoadDebt(url, httpClientSpy);
+  return {
+    sut,
+    httpClientSpy,
+  };
+};
+
 describe('RemoteLoadDebt', () => {
   test('Should call HttPClient with correct URL and method', async () => {
     const url = faker.internet.url();
-    const httpClientSpy = new HttpClientSpy();
-    const sut = new RemoteLoadDebt(url, httpClientSpy);
-    sut.load();
+    const { httpClientSpy, sut } = makeSut(url);
+    await sut.load();
     expect(httpClientSpy.url).toBe(url);
     expect(httpClientSpy.method).toBe('get');
   });
