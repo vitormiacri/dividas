66579c9a8c6decd80ecf533c519cc1691abec672
Vitor Morelli <vitao.miacri@gmail.com>
2021-08-07 17:51:53 -0300

    test: ensure SaveDebt returns RemoteSaveDebtModel on 200

--- src/data/test/mock-save-debt.ts
+++ src/data/test/mock-save-debt.ts
@@ -1,8 +1,13 @@
 import { SaveDebtParams } from '@/domain/usecases';
 import faker from 'faker';
+import { RemoteSaveDebtModel } from '../models';
 
 export const mockSaveDebtParams = (): SaveDebtParams => ({
   idUsuario: faker.datatype.number(),
   motivo: faker.random.words(),
   valor: faker.datatype.number(),
+});
+
+export const mockRemoteSaveDebtModel = (): RemoteSaveDebtModel => ({
+  success: faker.datatype.boolean(),
 });
--- src/data/usecases/save-debt/remote-save-debt.spec.ts
+++ src/data/usecases/save-debt/remote-save-debt.spec.ts
@@ -1,18 +1,53 @@
 import faker from 'faker';
 
-import { HttpClientSpy, mockSaveDebtParams } from '@/data/test';
+import {
+  HttpClientSpy,
+  mockRemoteSaveDebtModel,
+  mockSaveDebtParams,
+} from '@/data/test';
 import { RemoteSaveDebt } from './remote-save-debt';
+import { HttpStatusCode } from '@/data/protocols/http';
+
+type SutTypes = {
+  sut: RemoteSaveDebt;
+  httpClientSpy: HttpClientSpy;
+};
+
+const makeSut = (url = faker.internet.url()): SutTypes => {
+  const httpClientSpy = new HttpClientSpy();
+  const sut = new RemoteSaveDebt(url, httpClientSpy);
+  return {
+    sut,
+    httpClientSpy,
+  };
+};
 
 describe('RemoteSaveDebt', () => {
   test('Should call HttpClient with correct values', async () => {
     const url = faker.internet.url();
-    const httpClientSpy = new HttpClientSpy();
-    const sut = new RemoteSaveDebt(url, httpClientSpy);
+    const { sut, httpClientSpy } = makeSut(url);
+    httpClientSpy.response = {
+      statusCode: HttpStatusCode.ok,
+      body: mockRemoteSaveDebtModel(),
+    };
     const saveDebtParams = mockSaveDebtParams();
     const method = 'put';
     await sut.save(saveDebtParams, method);
     expect(httpClientSpy.url).toBe(url);
     expect(httpClientSpy.method).toBe(method);
     expect(httpClientSpy.body).toBe(saveDebtParams);
+  });
+
+  test('Should return true on 200', async () => {
+    const { sut, httpClientSpy } = makeSut();
+    const response = mockRemoteSaveDebtModel();
+
+    httpClientSpy.response = {
+      statusCode: HttpStatusCode.ok,
+      body: response,
+    };
+
+    const httpResponse = await sut.save(mockSaveDebtParams(), 'post');
+    expect(httpResponse).toBe(response.success);
   });
 });
