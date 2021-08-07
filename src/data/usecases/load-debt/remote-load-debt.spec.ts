cf399b2d0888d77438ec1bb2098efdbe364b8407
Vitor Morelli <vitao.miacri@gmail.com>
2021-08-07 15:34:19 -0300

    test: ensure RemoteLoadDebt returns DebtModel on 200

--- src/data/usecases/load-debt/remote-load-debt.spec.ts
+++ src/data/usecases/load-debt/remote-load-debt.spec.ts
@@ -1,11 +1,14 @@
-import { HttpClientSpy } from '@/data/test';
 import faker from 'faker';
+import { HttpClientSpy, mockRemoteDebtModel } from '@/data/test';
+import { RemoteDebtModel } from '@/data/models';
+import { HttpStatusCode } from '@/data/protocols/http';
 import { RemoteLoadDebt } from './remote-load-debt.';
 
 type SutTypes = {
   sut: RemoteLoadDebt;
-  httpClientSpy: HttpClientSpy;
+  httpClientSpy: HttpClientSpy<RemoteDebtModel>;
 };
+
 const makeSut = (url = faker.internet.url()): SutTypes => {
   const httpClientSpy = new HttpClientSpy();
   const sut = new RemoteLoadDebt(url, httpClientSpy);
@@ -19,8 +22,30 @@ describe('RemoteLoadDebt', () => {
   test('Should call HttPClient with correct URL and method', async () => {
     const url = faker.internet.url();
     const { httpClientSpy, sut } = makeSut(url);
+    httpClientSpy.response = {
+      statusCode: HttpStatusCode.ok,
+      body: mockRemoteDebtModel(),
+    };
     await sut.load();
     expect(httpClientSpy.url).toBe(url);
     expect(httpClientSpy.method).toBe('get');
+  });
+
+  test('Should return a Debt on 200', async () => {
+    const { httpClientSpy, sut } = makeSut();
+    const httpResult = mockRemoteDebtModel();
+    httpClientSpy.response = {
+      statusCode: HttpStatusCode.ok,
+      body: httpResult,
+    };
+    const httpResponse = await sut.load();
+    expect(httpResponse).toEqual({
+      _id: httpResult._id,
+      idUsuario: httpResult.idUsuario,
+      motivo: httpResult.motivo,
+      nome: httpResult.nome,
+      valor: httpResult.valor,
+      criado: new Date(httpResult.criado),
+    });
   });
 });
