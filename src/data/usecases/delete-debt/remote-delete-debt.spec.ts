5884857261eaa2e605db7c710b4cdfafcee2d6b0
Vitor Morelli <vitao.miacri@gmail.com>
2021-08-11 14:48:53 -0300

    test: ensure RemoteDeleteDebt throws UnexpectedError on 400

--- src/data/usecases/delete-debt/remote-delete-debt.spec.ts
+++ src/data/usecases/delete-debt/remote-delete-debt.spec.ts
@@ -1,6 +1,8 @@
 import faker from 'faker';
 import { HttpClientSpy } from '@/data/test';
 import { RemoteDeleteDebt } from './remote-delete-debt';
+import { HttpStatusCode } from '@/data/protocols/http';
+import { UnexpectedError } from '@/domain/erros/unexpected-error';
 
 type SutTypes = {
   sut: RemoteDeleteDebt;
@@ -25,5 +27,16 @@ describe('RemoteDeleteDebt', () => {
 
     expect(httpClientSpy.url).toBe(url);
     expect(httpClientSpy.method).toBe('delete');
+  });
+
+  test('Should throw UnexpectedError if HttpClient returns 400', async () => {
+    const { sut, httpClientSpy } = makeSut();
+    httpClientSpy.response = {
+      statusCode: HttpStatusCode.badRequest,
+    };
+
+    const promise = sut.delete();
+
+    await expect(promise).rejects.toThrow(new UnexpectedError());
   });
 });
